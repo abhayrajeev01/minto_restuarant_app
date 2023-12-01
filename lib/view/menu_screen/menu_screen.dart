9e58701ce4146b6b79366dd339f64b8fbd0fe86b
Abhay Rajeev <abhayrajeevkk2001@gmail.com>
2023-12-01 15:34:05 +0530

    update

--- lib/view/menu_screen/menu_screen.dart
+++ lib/view/menu_screen/menu_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minto_softronics/utils/menulist.dart';
+import 'package:lottie/lottie.dart';
+import 'package:minto_softronics/view/homescreen/homescreen.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key});
@@ -48,28 +50,45 @@ class _MenuScreenState extends State<MenuScreen> {
               },
             ),
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: ElevatedButton(
-              onPressed: () {
-                for (int i = 0; i < selectedValues.length; i++) {
-                  if (selectedValues[i]) {
-                    print(menuItems[i]);
-                  }
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: ElevatedButton(
+            onPressed: () {
+              for (int i = 0; i < selectedValues.length; i++) {
+                if (selectedValues[i]) {
+                  print(menuItems[i]);
                 }
-              },
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-                backgroundColor:
-                    MaterialStateProperty.all(Colors.grey.shade300),
-              ),
-              child: Text(
-                "Confirm",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
+              }
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Center(
+                    child: LottieBuilder.asset(
+                        "assets/Animation - 1701424738483.json"),
+                  );
+                },
+              );
+              Future.delayed(Duration(seconds: 3), () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => Homescreen(),
+                ));
+              });
+            },
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all(Colors.black),
+              backgroundColor: MaterialStateProperty.all(Colors.grey.shade300),
+            ),
+            child: Text(
+              "Confirm",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
